f572a1cb2d32c318e783ece9869d0843b6d619b5
nbkm8y5 <moreno.ronline@gmail.com>
2015-06-21 10:02:39 -0400

    Change to title

--- index.php
+++ index.php
@@ -25,7 +25,7 @@ mysql_close();
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>PHP Testing Center</title>
+    <title>BODY MASS INDEX CALCULATOR</title>
 
     <!-- Latest compiled and minified CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
